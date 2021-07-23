4e7374bc3110e884ccf4735eedf6e7555fde22d4
heyajustme <84226002+heyajustme@users.noreply.github.com>
2021-07-24 00:51:33 +0530

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -114,7 +114,7 @@ async def addcchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "MoMoMusic"
 
     try:
         await USER.join_chat(invitelink)
