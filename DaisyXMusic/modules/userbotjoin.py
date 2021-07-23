9cb4aa65f99b869e3048d9b7ac34842758b53173
heyajustme <84226002+heyajustme@users.noreply.github.com>
2021-07-24 00:50:33 +0530

    Update userbotjoin.py

--- DaisyXMusic/modules/userbotjoin.py
+++ DaisyXMusic/modules/userbotjoin.py
@@ -40,7 +40,7 @@ async def addchannel(client, message):
     try:
         user = await USER.get_me()
     except:
-        user.first_name = "DaisyMusic"
+        user.first_name = "MoMoMusic"
 
     try:
         await USER.join_chat(invitelink)
@@ -53,7 +53,7 @@ async def addchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your group due to heavy join requests for userbot! Make sure user is not banned in group."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @ADLMusicPlayer to your Group and try again</b>",
         )
         return
     await message.reply_text(
@@ -128,7 +128,7 @@ async def addcchannel(client, message):
         print(e)
         await message.reply_text(
             f"<b>🛑 Flood Wait Error 🛑 \n User {user.first_name} couldn't join your channel due to heavy join requests for userbot! Make sure user is not banned in channel."
-            "\n\nOr manually add @DaisyXhelper to your Group and try again</b>",
+            "\n\nOr manually add @ADLMusicPlayer to your Group and try again</b>",
         )
         return
     await message.reply_text(
